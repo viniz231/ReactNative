9efbf894b178eb469794ad48201c49a6bc584dc8
Vinicius Elohin <vinicius.7246@aluno.pr.senac.br>
2025-08-18 15:39:07 -0300

    Colocando TextInput e a img, arrumando algumas partes das telas

--- y/src/pages/login/index.tsx
+++ y/src/pages/login/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput, } from "react-native";
 import { style } from "./styles";
+import Logo from "../../assets/logo.png"
 
 export default function login() {
     return (
         <View style={style.container}>
         <View style={style.boxTop}>
         <Text>Top</Text>
+        <Image
+        source={Logo}
+        />
+        <Text>Bem vindo de volta</Text>
         </View>
         <View style={style.boxMid}>
         <Text>Mid</Text>
+        <Text>Endereço de E-mail</Text>
+        <TextInput />
+        <Text>Senha</Text>
+        <TextInput />
         </View>
         <View style={style.boxBotton}>
-        <Text>Botton</Text>
         </View>
-
         </View>
     )
 }
